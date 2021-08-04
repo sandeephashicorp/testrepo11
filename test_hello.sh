5eced45fd7bcec4378acc844821b35258ffe4070
Sandeep Rawat <srawat@hashicorp.com>
2021-08-04 16:33:48 +0530

    Create test_hello.sh

---
+++ test_hello.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+out=$(bash hello.sh)
+
+# we should check out == hello
+if [ "${out}" == "hello" ];then
+  echo "GOOD: test pass"
+else
+  echo "BAD: test fail"
+  echo "expected hello, got ${out}"
+  exit 1
+fi
